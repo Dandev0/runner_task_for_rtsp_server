2c0b61340a9b691eb548f52ae37ee290795dfa69
Administrator <admin@example.com>
2023-09-25 17:49:56 +0200

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,12 @@ FROM python:3.10
 COPY requirements.txt /app/
 WORKDIR /app
 RUN apt-get update
+RUN apt-get install -y ffmpeg
 RUN pip install -r requirements.txt
 ENV RABBITMQ_LOGIN=test
 ENV RABBITMQ_PASSWORD=test
 ENV RABBITMQ_IP=46.146.229.116
 ENV RABBITMQ_PORT=5672
-COPY ./runner_task_for_rtsp_server  /app
+COPY . /app
 WORKDIR /app
 CMD ["python3", "rabbit_listener.py"]
--- ffmpeg_runner.py
+++ ffmpeg_runner.py
@@ -3,17 +3,16 @@ import shlex
 
 
 class Ffmpeg_rtsp:
-    def __init__(self, rtsp_url: str = None, output_url: str = 'localhost'):
-        self.transport = '-rtsp_transport tcp'
+    def __init__(self, rtsp_url: str = None, output_url: str = 'localhost', transport='-rtsp_transport tcp'):
+        self.transport = transport
         self.__rtsp_url = rtsp_url
         self.__output_url = output_url
 
     def __str__(self):
-        return f'ffmpeg {self.transport} -i {self.__rtsp_url} -codec copy -f rtsp {self.__output_url}'
+        return f'ffmpeg {self.transport} -i {self.__rtsp_url} -codec copy -f flv -flvflags no_duration_filesize -f rtsp {self.__output_url}'
 
     def add_rtsp_camera_to_rtsp_server(self):
         args = shlex.split(self.__str__())
         p = subprocess.Popen(args, stdout=subprocess.PIPE)
         p.communicate()[0]
-
 
